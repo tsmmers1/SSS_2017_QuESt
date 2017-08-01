80dec05c59cd9de823cb93e58fc03c6843c3bba6
Daniel Smith <malorian@me.com>
2017-08-01 14:07:43 -0400

    Adds a few tests for trials

---
+++ tests/test_jk.py
@@ -0,0 +1,34 @@
+"""
+This file tests the JK file in the project.
+"""
+
+import project
+import pytest
+import psi4
+import numpy as np
+
+psi4.set_output_file("output.dat", True)
+
+def test_PKJK():
+    molecule = psi4.geometry("""
+O
+H 1 1.1
+H 1 1.1 2 104
+""")
+
+    basis = psi4.core.BasisSet.build(molecule, "ORBITAL", "STO-3G")
+    mints = psi4.core.MintsHelper(basis)
+    jk = project.jk.build_JK(mints, "PK")
+
+    Cocc = np.random.rand(mints.nbf(), 5)
+    D = np.dot(Cocc, Cocc.T)
+
+    I = np.asarray(mints.ao_eri())
+    J_ref = np.einsum("pqrs,rs->pq", I, D)
+    K_ref = np.einsum("prqs,rs->pq", I, D)
+
+    J, K = jk.compute_JK(Cocc)
+    assert np.allclose(J_ref, J)
+    assert np.allclose(K_ref, K)
+
+    
