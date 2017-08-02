f09fbff5b0c2dfe5642c504be0711b8c4f228820
semodi <sebastian-dick@gmx.net>
2017-08-02 16:43:27 -0400

    loop through molecules in test_jk.py

--- tests/test_jk.py
+++ tests/test_jk.py
@@ -7,27 +7,25 @@ import pytest
 import psi4
 import numpy as np
 
+
 psi4.set_output_file("output.dat", True)
 
 
 def test_PKJK():
-    molecule = psi4.geometry("""
-O
-H 1 1.1
-H 1 1.1 2 104
-""")
+    for mol_str in quest.mollib:
+        molecule = psi4.geometry(mol_str)
 
-    basis = psi4.core.BasisSet.build(molecule, "ORBITAL", "STO-3G")
-    mints = psi4.core.MintsHelper(basis)
-    jk = quest.jk.build_JK(mints, "PK")
+        basis = psi4.core.BasisSet.build(molecule, "ORBITAL", "STO-3G")
+        mints = psi4.core.MintsHelper(basis)
+        jk = quest.jk.build_JK(mints, "PK")
 
-    Cocc = np.random.rand(mints.nbf(), 5)
-    D = np.dot(Cocc, Cocc.T)
+        Cocc = np.random.rand(mints.nbf(), 5)
+        D = np.dot(Cocc, Cocc.T)
 
-    I = np.asarray(mints.ao_eri())
-    J_ref = np.einsum("pqrs,rs->pq", I, D)
-    K_ref = np.einsum("prqs,rs->pq", I, D)
+        I = np.asarray(mints.ao_eri())
+        J_ref = np.einsum("pqrs,rs->pq", I, D)
+        K_ref = np.einsum("prqs,rs->pq", I, D)
 
-    J, K = jk.compute_JK(Cocc)
-    assert np.allclose(J_ref, J)
-    assert np.allclose(K_ref, K)
+        J, K = jk.compute_JK(Cocc)
+        assert np.allclose(J_ref, J)
+        assert np.allclose(K_ref, K)
