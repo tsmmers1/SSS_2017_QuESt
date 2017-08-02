28ae1df00ddc7a9ad42451a99650bb4b5845c3ef
Kate Lebold <kml5480@psu.edu>
2017-08-02 11:53:52 -0400

    Updated molecule class with better documentation

--- quest/molecule.py
+++ quest/molecule.py
@@ -1,20 +1,51 @@
+# This file defines a molecule class that returns information about the molecule. 
+# It also contains member functions set_geometry(str) for setting a geometry from a 
+#   string or psi4 geometry, and set_basis(str) for setting up a basis set with psi4.
+
 import psi4
 
 class Molecule(object):
-    '''
-    A molecule class that is used to store basic parameters including
+    """
+    A molecule class that is stores basic parameters, including
     psi4 geometry, basis set, number of electrons, and number of doubly
     occupied orbitals.
-    '''
+
+    """
 
     def __init__(self, mol=None, bas=None):
-        '''
-        Initialize molecule with geometry mol and basis
-        set bas. Calculate number of electrons nel and
-        number of doubly occupied orbitals ndocc.
-        '''
+        """
+        Initializes molecule with geometry mol and basis set bas.
+        Calculate number of electrons nel and number of doubly occupied orbitals ndocc.
+
+        Parameters
+        ----------
+        mol : str or psi4.core.Molecule
+              psi4 geometry
+        bas : str
+              basis set
+
+        Returns
+        -------
+        A built molecule
+
+        Examples
+        --------
+        molecule = Molecule.mol()
+        basis_set = Molecule.bas()
+        num_electrons = Molecule.nel()
+        num_doub_occ_orbs = Molecule.ndocc()
+
+        """
+        
         self.mol = mol
         self.bas = bas
+
+        # Sets molecule and basis set
+        if bas is not None:
+            self.set_basis(bas)
+
+        if mol is not None:
+            self.set_geometry(mol)
 
         # Calculate the number of doubly occupied orbitals and the number of electrons
         self.nel = sum(self.molecule.Z(n) for n in range(self.molecule.natom()))
@@ -25,10 +56,24 @@ class Molecule(object):
         self.ndocc = int(self.nel / 2.0)
 
     def set_geometry(self, geom_str):
-        '''
-        Set the geometry of the molecule by providing
-        a psi4 style string or psi4 geometry.
-        '''
+        """
+        Set the geometry of the molecule (by providing a psi4 style string or psi4 geometry).
+
+        Parameters
+        ----------
+        geom_str : str
+                   psi4-style geometry string (or psi4 geometry)
+
+        Returns
+        -------
+        A psi4 geometry
+
+        Examples
+        --------
+        Molecule.set_geometry("He")
+
+        """
+
         if isinstance(mol, geom_str):
             self.mol = psi4.geometry(mol)
         elif isinstance(mol, psi4.core.Molecule):
@@ -39,9 +84,23 @@ class Molecule(object):
         self.mol.update_geometry()
 
     def set_basis(bas_str):
-        '''
-        set the basis set that is used in any further calculations
-        '''
+        """
+        Set the basis set.
+
+        Parameters
+        ----------
+        bas_str : str
+                  basis set name
+
+        Returns
+        -------
+        A basis set
+
+        Examples
+        --------
+        Molecule.set_basis("STO-3G")
+
+        """
         if(self.mol is None):
             raise Exception('Error: Geometry not defined')
         else:
