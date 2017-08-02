5cbffded276e643c9322a12a7b4acc1de2a0896a
Daniel Smith <malorian@me.com>
2017-08-02 15:48:06 -0400

    Fixes molecule

--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -9,19 +9,6 @@ import numpy as np
 
 
 def test_molecule():
-    molecule = psi4.geometry("""
-O
-H 1 1.1
-H 1 1.1 2 104
-""")
-
-    basis = "sto-3g"
-
-    testmol = quest.molecule.Molecule()
-
-    testmol.set_geometry("molecule")
-
-    testmol.set_basis("sto-3g")
-
+    testmol = quest.molecule.Molecule(quest.mollib["h2o"], "sto-3g")
     assert testmol.nel == 10
 
