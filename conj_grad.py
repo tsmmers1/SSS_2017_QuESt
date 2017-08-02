cdb40b5bf5b6a852c8e43561d13ca8886615c819
Ashutosh Kumar <akumar1@vt.edu>
2017-08-02 14:32:36 -0400

    matching results with numpy's linear solver

--- conj_grad.py
+++ conj_grad.py
@@ -39,10 +39,12 @@ def helper_PCG_direct(A, b, tol=1e-10, max_iter=None, x0=None, M=None):
         max_iter = 2*len(b)
     if x0 is None:
         x = np.zeros_like(b)
+    else:
+        x = x0
     if M is None:
         M = np.ones_like(A)
 
-    r = b -np.dot(A,x)
+    r = b - np.dot(A,x)
     z = r/np.diag(M)
     n = len(b)
     p = z
@@ -59,23 +61,31 @@ def helper_PCG_direct(A, b, tol=1e-10, max_iter=None, x0=None, M=None):
         res_k_norm = res_kplus1_norm
         rms = np.sqrt(np.sum(r**2) / len(r))
         print('CG Iteration %3d: RMS = %3.8f' % (iteration, rms))
-        if res_kplus1_norm < tol or iteration > max_iter:
-            #print('Total iterations:', i)
+        if (res_kplus1_norm < tol) or (iteration > max_iter):
+            print('\nConverged!!\n')
             break
         p = beta * p + z
     return x
 
 
 if __name__ == '__main__':
+    # Generate sample symmetric positive-definite matrix A and RHS vector b
     n = 100
-    tol = 1e-14
     A = np.random.normal(size=[n, n])
     A = np.dot(A.T, A)
     b = np.ones(n)
-    #xo = np.zeros(n)
-    #x = helper_CG(A, b)
+    max_iteration = 2 * n
+
+    # Numpy's linear equation solver
     x = np.linalg.solve(A, b)
+
+    # Our implementation
+    tol = 1e-14
+    x0 = np.zeros(n)
     M = np.ones_like(A)
-    x1 = helper_PCG(A, b, tol, x0, M)
+    x1 = helper_PCG_direct(A, b, tol, max_iteration, x0, M)
+
+    # Check our solution vs. numpy
     print(" Solution matches with numpy's cg solver: %s" % np.allclose(x, x1, 1e-9))
+    #assert(np.allclose(x, x1, 1e-9))
 
