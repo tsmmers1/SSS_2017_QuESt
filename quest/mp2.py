9efb1f5ee9d769d6a61019a22f63f144aca1f411
Andrew M. James <amjames2@vt.edu>
2017-08-02 10:54:41 -0400

    MP2: adding framework

---
+++ quest/mp2.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+
+def mp2(wfn):
+    pass
+
+
+def df_mp2(wfn):
+    pass
+
+
+def _mo_transform():
+    pass
+
+
+def _denom():
+    pass
+
+
+def _compute_conv_e():
+    pass
+
+
+def _compute_DF_e():
+    pass
