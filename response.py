24954ab5613ec5d6d061374d0feed126d7f24a89
Nadav <gevahn@mit.edu>
2017-08-02 14:04:58 -0400

    added ao to mo transforms

--- response.py
+++ response.py
@@ -3,11 +3,28 @@ import scipy.sparse.linalg as spla
 import scipy as sp
 
 
+
+
+def ao_to_mo(tensor, transform):
+    if len(g.shape) == 2:
+        return transform.T @ tensor @ transform
+    if len(g.shape) == 4:
+        tensor = np.einsum('pqrs,pt->tqrs', tensor, transform)
+        tensor = np.einsum('pqrs,qt->ptrs', tensor, transform)
+        tensor = np.einsum('pqrs,rt->pqts', tensor, transform)
+        tensor = np.einsum('pqrs,st->pqrt', tensor, transform)
+        return tensor
+    raise Exception("Not a 2 or 4 tensor")
+
 def response(g, F, C, L, R, nocc):
     '''
     Calculates the CPHF response.
     Expects the eri 4-tensor, g,  the Fock matrix, F, the MO coeefcient matrix, C, the left and right response tensors, L and R, and the occupation number, nocc.
     '''
+
+    F = ao_to_mo(F, C)
+    g = ao_to_mo(g, C)
+
     nbas = F.shape[0]
     nvirt = nbas - nocc 
     opeartor_vs = 1
