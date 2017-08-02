10bde48f3c4b4e3d94e74609b4d9436a756d5ce1
kbsezginel <kbs37@pitt.edu>
2017-08-02 17:05:58 -0400

    Add MP2 to CLI and MP2 and SCF response to driver

--- quest/driver.py
+++ quest/driver.py
@@ -6,10 +6,16 @@ from . import mp2
 from . import scf_module
 from . import molecule
 from . import wavefunction
+from .response import response
 
 
-#def compute_mp2(molecule, basis="aug-cc-pvdz"):
-#	mp2.mp2(molecule, basis)
+def compute_scf_response(wavefunction):
+    return response(wavefunction)
+
+
+def compute_mp2(wavefunction):
+    return mp2.mp2(wavefunction)
+
 
 def compute_rhf(mol, basis_name="aug-cc-pvdz", numpy_memory=1.e9, maxiter=12, E_conv=1.e-6, D_conv=1.e-4):
     mol = molecule.Molecule(mol, basis_name)
@@ -21,4 +27,4 @@ def compute_rhf(mol, basis_name="aug-cc-pvdz", numpy_memory=1.e9, maxiter=12, E_
 
 
 def compute_mc(sigma, epsilon, coord_file_path, params):
-	pass
+    pass
