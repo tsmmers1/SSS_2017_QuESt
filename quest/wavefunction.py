dabcb23fbaef520f04cd6f61142d653c53cbb5fd
Patrick Avery <psavery@buffalo.edu>
2017-08-02 11:50:47 -0400

    Cleaned up WaveFunction to just contain the essential members

--- quest/wavefunction.py
+++ quest/wavefunction.py
@@ -2,28 +2,37 @@
 A basic wave function class
 """
 
-import numpy as np
 import psi4
 
+
 class WaveFunction(object):
-    """Basic wave function class"""
+    """
+    Basic wave function class
 
-    def __init__(self, psi4mol, basisname="sto-3g", scf_type="df",
-                 e_conv=1.e-8, d_conv=1.e-8):
-        psi4.set_options({'basis': basisname,
-                          'scf_type': scf_type,
-                          'e_convergence': e_conv,
-                          'd_convergence': d_conv})
+    Members:
+        options: dictionary with various options, including the basis name.
+        mints: contains a psi4 mints object built from the basis set.
+        energies: a dictionary of various energies that have been
+                  calculated (including scf, nuclear, mp2, etc.).
+        arrays: a dictionary of various arrays (including a coefficient
+                matrix, a fock matrix, etc.)
+    """
 
-        bas = psi4.core.BasisSet.build(psi4mol, target=basis)
+    def __init__(self, mol):
+        """
+        Initialize the WaveFunction class.
+
+        Args:
+            mol: a Molecule class from the QuESt repository. Needs to
+                 have a basis set at least.
+        """
+
+        # Set whichever options you would like. The only default is the
+        # basis set name.
+        self.options = {'basis_name': mol.bas.name}
 
         # Build the mints object
-        self.mints = psi4.core.MintsHelper(bas)
-
-        self.basisname = basisname
-        self.scf_type = scf_type
-        self.e_conv = e_conv
-        self.d_conv = d_conv
+        self.mints = psi4.core.MintsHelper(mol.bas)
 
         # Energies: scf_energy, mp2_os_corr, mp2_ss_corr, E_nuc
         self.energies = {}
