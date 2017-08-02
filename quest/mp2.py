adbeccb8744c011bc17e4d2c6176836cf8e4a254
aerask <aerask@umich.edu>
2017-08-02 12:03:57 -0400

    Conv E

--- quest/mp2.py
+++ quest/mp2.py
@@ -18,8 +18,16 @@ def _denom():
     pass
 
 
-def _compute_conv_e():
-    pass
+def _compute_conv_e(I_iajb, D_ijab):
+    # OS = (ia|jb)(ia|jb)/(ei+ej-ea-eb)
+    OS = np.einsum("iajb,iajb,ijab->", I_iajb, I_iajb, D_ijab)
+    # build (ib|ja)
+    I_ibja = I_iajb.swapaxes(1, 3)
+    # SS = [(ia|jb)-(ib|ja)](ia|jb)/(ei+ej-ea-eb)
+    SS = np.einsum("iajb,iajb,ijab->", (I_iajb - I_ibja), I_iajb, D_ijab)
+    # opposite spin, same spin
+    E_mp2 = OS - SS
+    return E_mp2
 
 
 def _compute_DF_e():
