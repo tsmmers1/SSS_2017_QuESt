afec0b8ca0fc6c541441d0022077f479ea5a39c7
Nadav <gevahn@mit.edu>
2017-08-02 11:51:56 -0400

    made response into a generic function

--- response.py
+++ response.py
@@ -4,29 +4,18 @@ import scipy.sparse.linalg as spla
 import scipy as sp
 import os
 
-def response(ao_ints, F, C, L_response, R_response, scf_params):
-    g = ao_ints['g4']
-    nbas = scf_params['nbas']
-    nocc = scf_params['nel']
-    nvirt = nbas - nocc
-    F = xform_2(F,C)
-    g = xform_4(g,C)
-    L = np.zeros((3,nocc * nvirt))
-    R = np.zeros((3,nocc * nvirt))
-    for i in range(3):
-        L[i,:] = xform_2(2*L_response[i], C)[:nocc,nocc:].ravel()
-        R[i,:] = xform_2(2*R_response[i], C)[:nocc,nocc:].ravel()
-
-
-    E = np.zeros((nbas, nbas, nbas, nbas))
-    E += np.einsum('ij,ab->iajb',np.eye(nbas),F)
-    E -= np.einsum('ab,ij->iajb',np.eye(nbas),F)
-
-    E += 4*np.einsum('aijb->iajb',g) - np.einsum("jiab->iajb",g) - np.einsum("ajbi->iajb",g)
-
-    E = E[:nocc, nocc:, :nocc, nocc:]
-    E = E.reshape(nocc * nvirt, nocc * nvirt)
-    E_inv_R = np.linalg.solve(E,R.T)
+def response(g, F, C, L, R, nocc):
+    nbas = F.shape[0]
+    nvirt = nbas - nocc 
+    opeartor_vs = 1
+    if operator_vs == 0:
+        E_inv_R = np.linalg.solve(E,R.T)
+    elif operator_vs == 1:
+        E_kappa = E_kappa_MO(F, g, nocc, nvirt)
+        E_inv_R = spla.cg(E_kappa, R)
+    elif operator_vs == 2:
+        E_kappa = E_kappa_MO(F, g, C, get_JK, nocc, nvirt)
+        E_inv_R = spla.cg(E_kappa, R)
     return np.dot(L, E_inv_R) 
 
 
