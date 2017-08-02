676faaf2ce96b6eb3c73f3f79f49c35e5b653906
hongzhouye <hzye2011@mit.edu>
2017-08-02 12:03:07 -0400

    Separate kappa from response

--- response.py
+++ response.py
@@ -1,32 +1,31 @@
 import numpy as np
-from scf_utils import *
 import scipy.sparse.linalg as spla
-import scipy as sp
-import os
 
-def response(ao_ints, F, C, L_response, R_response, scf_params):
-    g = ao_ints['g4']
-    nbas = scf_params['nbas']
-    nocc = scf_params['nel']
+# modules that need interaction w/ other grps
+from scf_utils import xform_2, xform_4
+
+def response(g, C, F, nbas, nocc):
     nvirt = nbas - nocc
-    F = xform_2(F,C)
-    g = xform_4(g,C)
-    L = np.zeros((3,nocc * nvirt))
-    R = np.zeros((3,nocc * nvirt))
-    for i in range(3):
-        L[i,:] = xform_2(2*L_response[i], C)[:nocc,nocc:].ravel()
-        R[i,:] = xform_2(2*R_response[i], C)[:nocc,nocc:].ravel()
+    F = xform_2(F, C)
+    g = xform_4(g, C)
+    print(nbas, nocc, F.shape, g.shape)
 
+    # original version
+    E0 = np.zeros([nbas, nbas, nbas, nbas])
+    E0 += np.einsum('ij,ab->iajb', np.eye(nbas), F)
+    E0 -= np.einsum('ab,ij->iajb', np.eye(nbas), F)
+    E0 += 4 * np.einsum('aijb->iajb', g) - np.einsum("jiab->iajb", g) - \
+        np.einsum("ajbi->iajb", g)
+    E0 = E0[:nocc, nocc:, :nocc, nocc:]
 
-    E = np.zeros((nbas, nbas, nbas, nbas))
-    E += np.einsum('ij,ab->iajb',np.eye(nbas),F)
-    E -= np.einsum('ab,ij->iajb',np.eye(nbas),F)
-
-    E += 4*np.einsum('aijb->iajb',g) - np.einsum("jiab->iajb",g) - np.einsum("ajbi->iajb",g)
-
-    E = E[:nocc, nocc:, :nocc, nocc:]
+    # concise version
+    """
+    E = -F.reshape(1, nbas, 1, nbas)[:, :nvirt, :, :nvirt] + \
+        F.reshape(nbas, 1, nbas, 1)[:nocc, :, :nocc, :] + \
+        4. * g[:nocc, :nvirt, :nocc, :nvirt] - \
+        g[:nocc, :nocc, :nvirt, :nvirt].swapaxes(1, 2) -\
+        g[:nvirt, :nocc, :nvirt, :nocc].swapaxes(0, 3).swapaxes(1, 2)
     E = E.reshape(nocc * nvirt, nocc * nvirt)
-    E_inv_R = np.linalg.solve(E,R.T)
-    return np.dot(L, E_inv_R) 
+    """
 
-
+    return E
