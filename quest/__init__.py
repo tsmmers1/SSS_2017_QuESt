f6bfb34f1368368923b5718f4c11f83b80447da8
Daniel Smith <malorian@me.com>
2017-08-02 16:25:14 -0400

    Fixes up CLI

--- quest/__init__.py
+++ quest/__init__.py
@@ -21,3 +21,6 @@ import os
 if "OMP_NUM_THREADS" in list(os.environ):
     psi4.set_num_threads(int(os.environ["OMP_NUM_THREADS"]))
 psi4.set_output_file("psi_output.out")
+
+# Import default params
+default_params = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'default_params.yml')
